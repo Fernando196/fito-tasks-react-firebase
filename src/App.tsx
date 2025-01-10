6832e7220f23aeebff7522cd1c8f0658ded39afe
Fernando196 <fer_jose1996@hotmail.com>
2025-01-09 21:55:28 -0600

    refactor App component to use AppRouter for improved routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-// import { useState } from 'react'
 import './App.scss'
+import { AppRouter } from './routes'
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <AppRouter />
     </>
   )
 }
